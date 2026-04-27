3204041a2131dc4579cb97dd893c0185136bac13
binoymanoj <binoypm2002@gmail.com>
2026-04-27 22:56:02 +0530

    update: neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,18 @@
 
 -- Lazy.nvim bootstrap
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-  })
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 -- Minimal number of screen lines to keep above and below the cursor.
